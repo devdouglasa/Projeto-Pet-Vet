e32cf93075ce53e41b0294092e5af252f989220e
DevAllyk <douglasalisson27@gmail.com>
2022-10-09 12:06:53 -0300

    projeto pet vet

---
+++ pet-vet/Models/Agendamento.cs
@@ -0,0 +1,11 @@
+﻿namespace pet_vet.Models
+{
+    public class Agendamento
+    {
+        public string nome { get; set; }
+        public string contato { get; set; }
+        public string data { get; set; }
+        public string animal { get; set; }
+        public string necessidade { get; set; }
+    }
+}
---
+++ pet-vet/Models/Dados.cs
@@ -0,0 +1,21 @@
+﻿namespace pet_vet.Models
+{
+    public class Dados
+    {
+        public static Agendamento AgendamentoAtual = new Agendamento();
+
+        public static void Inserir(Agendamento agenda)
+        {
+            AgendamentoAtual.nome = agenda.nome;
+            AgendamentoAtual.data = agenda.data;
+            AgendamentoAtual.contato = agenda.contato;
+            AgendamentoAtual.animal = agenda.animal;
+            AgendamentoAtual.necessidade = agenda.necessidade;
+        }
+
+        public static Agendamento Listar()
+        {
+            return AgendamentoAtual;
+        }
+    }
+}
